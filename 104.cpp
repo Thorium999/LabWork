b25e4d1d8e25b86ab8a1d4aea6d410bfd4b08d4a
Thorium999 <Thorium_90@163.com>
2025-11-13 19:15:28 +0800

    v1.1

---
+++ 104.cpp
@@ -0,0 +1,23 @@
+/*
+		LabWork#104
+		观察不同数据类型对输出结果的影响
+		2025.11.12
+*/
+#include<iostream>
+#include<iomanip>
+int main()
+{
+	unsigned int testUnint = 65534;//0xFFFE
+	std::cout << "output 65534 in..."
+		<< "\n\tunsigned int type :\t " << testUnint
+		<< "\n\tOct unsigned int type:\t" << std::oct << testUnint << std::dec
+		<< "\n\tchar type:" << static_cast<char>(testUnint)
+		//char截取低8位,变为0xFE,即254,对应的ASCII字符是非打印字符,什么都不会显示
+		<< "\n\tshort type:\t\t" << static_cast<short>(testUnint)
+		//有符号的short范围是-32768到32767,这里发生补码
+		<< "\n\tint type:\t\t" << static_cast<int>(testUnint)
+		<< "\n\tdouble type:\t\t" << static_cast<double>(testUnint)
+		<< "\n\tdouble type (4):\t" << std::setprecision(4) << static_cast<double>(testUnint)
+		<< "\n\tHex unsigned int type:\t" << std::hex << testUnint << std::dec;
+	return 0;
+}
---
+++ 201.cpp
@@ -0,0 +1,32 @@
+/*
+		LabWork#201
+		输入一个字符,如果为小写,转换为大写输出,否则,输出其后继字符的ASCII码值.
+		2020.11.13
+*/
+#include<iostream>
+#include<string>
+int main()
+{
+	std::string ipt;
+	std::cout << "Awaiting input (1 single character)\n";
+	getline(std::cin, ipt);
+	//检查是否为单个字符
+	if (ipt.length() != 1)
+	{
+		std::cout << "ERROR#1:not a single character!";
+		return 1;
+	}
+	//取第一个
+	char fst = ipt[0];
+	if (fst >= 'a' && fst <= 'z')
+	{
+		char cap = fst - 32;
+		std::cout << cap;
+	}
+	else
+	{
+		int nxt = fst + 1;
+		std::cout << nxt;
+	}
+	return 0;
+}
